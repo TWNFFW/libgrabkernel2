722e7787975253289eb4dd77a6e7e3139580f229
TWNFFW <65101380+TWNFFW@users.noreply.github.com>
2024-10-01 00:54:56 +0800

    Update appledb.m

--- src/appledb.m
+++ src/appledb.m
@@ -13,7 +13,6 @@
 #import <sys/sysctl.h>
 #import "utils.h"
 
-// Change const with my url
 #define FIXED_FIRMWARE_URL @"https://updates.cdn-apple.com/2022FallFCS/patches/012-91710/FF24EECC-A4AD-4A8E-8BB2-2883017CBB00/com_apple_MobileAsset_SoftwareUpdate/afcfcefd48dcc09888e57f0584049c848881b410.zip"
 
 NSString *getFirmwareURL(bool *isOTA) {
